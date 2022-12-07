7ffbd762fb1dacede178668e4f461030aee9ced8
MattZKruspe <matthewmarquez2003@gmail.com>
2022-12-07 10:28:53 -0600

    first commit

---
+++ src/main/java/com/mycompany/catproject/Gato.java
@@ -0,0 +1,41 @@
+package com.mycompany.catproject;
+
+/**
+ *
+ * @author Matthew Marquez
+ */
+
+public class Gato {
+    private String id;
+    private String url;
+    private String image;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+    
+    
+    
+}
